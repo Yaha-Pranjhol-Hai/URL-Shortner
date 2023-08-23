26b9beec1ae19678bd176f478d70a99dbe6ffd8b
Pranjal <jainpranjal30112002@gmail.com>
2023-08-23 18:54:01 +0530

    [refactor] resolved error for number of clicks.

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { URL } = require("./models/url");
 const { connectMongoDB } = require("./connection");
 
 const urlRoute = require("./routes/url");
 const app = express();
 const PORT = 8001;
 
+connectMongoDB("mongodb://127.0.0.1:27017/URL-Shortner")
+.then(() => console.log(`Connected to MongoDB.`))
+
 app.use(express.json());
 
 app.use("/url", urlRoute);
 
-connectMongoDB("mongodb://127.0.0.1:27017/URL-Shortner")
-.then(() => console.log(`Connected to MongoDB.`))
-
+app.get('/:shortId', async (req,res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId,
+    },
+    {
+        $push: {
+            visitHistory: {
+                timestamp: Date.now(),
+            }
+        }
+    }
+    );
+    res.redirect(entry.redirectURL);
+})
 
 app.listen(PORT, () => {
     console.log(`Server started at PORT: 8001`);
